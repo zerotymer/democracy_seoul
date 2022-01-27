348c1434d72da4b460ca3b090188bb748fbd6b25
NJK <hibrobro@nate.com>
2022-01-27 18:13:01 +0900

    JK-Member

---
+++ src/main/java/kr/go/seoul/democracy/member/model/service/MemberService.java
@@ -0,0 +1,11 @@
+package kr.go.seoul.democracy.member.model.service;
+
+import kr.go.seoul.democracy.common.model.vo.Member;
+
+public interface MemberService {
+	
+	
+	Member selectLoginMember(Member member);
+
+
+}
