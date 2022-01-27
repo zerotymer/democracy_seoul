52f25d4271dcb1113862dde1171b2d9489edb838
zerotymer <zerotymer+github@gmail.com>
2022-01-27 13:44:47 +0900

    2022-01-27 오후 1:44:38 | aop 수정

--- src/main/java/kr/go/seoul/common/EncryptAdvice.java
+++ src/main/java/kr/go/seoul/common/EncryptAdvice.java
@@ -1,10 +1,47 @@
 package kr.go.seoul.common;
 
+import kr.go.seoul.democracy.common.model.vo.Member;
 import org.aspectj.lang.JoinPoint;
 
-public class EncryptAdvice {
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
-	public void encryptPassword(JoinPoint jp) {
-		
+public class EncryptAdvice {
+	/// FIELDs
+	private EncryptionTemplate template;
+
+	/// CONSTRUCTORs
+	public EncryptAdvice(EncryptionTemplate template) {
+		this.template = template;
+	}
+	
+	/**
+	 * 사용자의 Password를 암호화
+	 * @param jp: data(Member.userPwd), salt(Member.userId)
+	 * @throws NoSuchAlgorithmException
+	 */
+	public void encryptPasswordForMember(JoinPoint jp) throws NoSuchAlgorithmException {
+		if (!(jp.getArgs()[0] instanceof Member)) return;
+
+		Member member = jp.getArgs()[0] == null ? null : (Member) jp.getArgs()[0];
+		if (member == null) return;
+
+		String data = this.template.encryptString(member.getUserPwd(), member.getUserId());
+		member.setUserPwd(data);
+	}
+
+	/**
+	 * 사용자의 Password를 암호화
+	 * @param jp
+	 */
+	public void encryptPasswordForHashMap(JoinPoint jp) throws NoSuchAlgorithmException {
+		if (!(jp.getArgs()[0] instanceof HashMap)) return;
+		HashMap<String, Object> map = (HashMap<String, Object>) jp.getArgs()[0];
+
+		String userId = (String) map.get("userId");
+		String userPwd = (String) map.get("userPwd");
+
+		String data = this.template.encryptString(userPwd, userId);
+		map.put("userPwd", data);
 	}
 }
