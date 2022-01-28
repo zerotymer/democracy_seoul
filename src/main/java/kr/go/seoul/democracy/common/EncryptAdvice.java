0c757c0e662c6ab36399d5230b3516bd6a5433e6
zerotymer <zerotymer+github@gmail.com>
2022-01-28 11:12:19 +0900

    2022-01-28 오전 11:12:07 | aop-context  문제 처리

--- src/main/java/kr/go/seoul/democracy/common/EncryptAdvice.java
+++ src/main/java/kr/go/seoul/democracy/common/EncryptAdvice.java
@@ -1,6 +1,10 @@
 package kr.go.seoul.democracy.common;
 
-import kr.go.seoul.common.EncryptionTemplate;
+import kr.go.seoul.democracy.common.model.vo.Member;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+
 import org.aspectj.lang.JoinPoint;
 
 /**
@@ -8,15 +12,41 @@ import org.aspectj.lang.JoinPoint;
  * @author 신현진
  */
 public class EncryptAdvice {
-    /// FIELDs
-    private EncryptionTemplate template;
+	/// FIELDs
+		private EncryptionTemplate template;
 
-    /// CONSTRUCTORs
-    public EncryptAdvice(EncryptionTemplate template) {
-        this.template = template;
-    }
+		/// CONSTRUCTORs
+		public EncryptAdvice(EncryptionTemplate template) {
+			this.template = template;
+		}
+		
+		/**
+		 * 사용자의 Password를 암호화
+		 * @param jp: data(Member.userPwd), salt(Member.userId)
+		 * @throws NoSuchAlgorithmException
+		 */
+		public void encryptPasswordForMember(JoinPoint jp) throws NoSuchAlgorithmException {
+			if (!(jp.getArgs()[0] instanceof Member)) return;
 
-    /// METHODs
-    public void encryptPassword(JoinPoint jp) {
-    }
+			Member member = jp.getArgs()[0] == null ? null : (Member) jp.getArgs()[0];
+			if (member == null) return;
+
+			String data = this.template.encryptString(member.getUserPwd(), member.getUserId());
+			member.setUserPwd(data);
+		}
+
+		/**
+		 * 사용자의 Password를 암호화
+		 * @param jp
+		 */
+		public void encryptPasswordForHashMap(JoinPoint jp) throws NoSuchAlgorithmException {
+			if (!(jp.getArgs()[0] instanceof HashMap)) return;
+			HashMap<String, Object> map = (HashMap<String, Object>) jp.getArgs()[0];
+
+			String userId = (String) map.get("userId");
+			String userPwd = (String) map.get("userPwd");
+
+			String data = this.template.encryptString(userPwd, userId);
+			map.put("userPwd", data);
+		}
 }
