e026c68584d373e79b37f321b9dd93b4d4ab9238
zerotymer <zerotymer+github@gmail.com>
2022-01-30 21:38:02 +0900

    2022-01-30 오후 9:37:53 | oracle DB 로 변경

--- src/main/java/kr/go/seoul/democracy/test/controller/TestController.java
+++ src/main/java/kr/go/seoul/democracy/test/controller/TestController.java
@@ -1,5 +1,30 @@
 package kr.go.seoul.democracy.test.controller;
 
-public class TestController {
+import kr.go.seoul.democracy.test.model.service.TestService;
+import kr.go.seoul.democracy.test.model.service.TestServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller("testController")
+public class TestController {
+    /// FIELDs
+    private TestService testService;
+
+    /// CONSTRUCTORs
+    @Autowired
+    public TestController(@Qualifier("testServiceImpl") TestServiceImpl testService) {
+        this.testService = testService;
+    }
+
+    /// METHODs
+    @RequestMapping("/test.do")
+    public String test() {
+    	System.err.println("test");
+        System.err.println(testService.selectSysdate());
+        System.err.println("test");
+        return "index";
+    }
 }
