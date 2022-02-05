b88c70be318de87f3359ea40af57697fcf4c1b43
zerotymer <zerotymer+github@gmail.com>
2022-02-05 20:46:21 +0900

    2022-02-05 오후 8:46:12 | 리스트 페이지 작성

--- src/main/java/kr/go/seoul/democracy/test/controller/TestController.java
+++ src/main/java/kr/go/seoul/democracy/test/controller/TestController.java
@@ -1,20 +1,12 @@
 package kr.go.seoul.democracy.test.controller;
 
 import kr.go.seoul.common.FileTransferTemplate;
-import kr.go.seoul.common.transfer.FileTransferInfo;
-
+import kr.go.seoul.democracy.test.model.service.TestService;
+import kr.go.seoul.democracy.test.model.service.TestServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import kr.go.seoul.democracy.test.model.service.TestService;
-import kr.go.seoul.democracy.test.model.service.TestServiceImpl;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller("testController")
 public class TestController {
@@ -42,6 +34,22 @@ public class TestController {
         return "test/content-frame";
     }
 
+    @RequestMapping("/test/content-list.do")
+    public String test3() {
+        return "test/content-list";
+    }
 
+    @RequestMapping("/test/suggest.do")
+    public String test4() {
+        return "test/list-suggest";
+    }
+    @RequestMapping("/test/discuss.do")
+    public String test5() {
+        return "test/list-discuss";
+    }
+    @RequestMapping("/test/survey.do")
+    public String test6() {
+        return "test/list-survey";
+    }
 
 }
