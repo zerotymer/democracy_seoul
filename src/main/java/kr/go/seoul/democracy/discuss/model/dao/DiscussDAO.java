4fc69ebb8d51ae6b01d24c91c96f4af8e3ee5073
codevlet <alice78651@gmail.com>
2022-02-10 01:03:43 +0900

    충돌주의

--- src/main/java/kr/go/seoul/democracy/discuss/model/dao/DiscussDAO.java
+++ src/main/java/kr/go/seoul/democracy/discuss/model/dao/DiscussDAO.java
@@ -1,6 +1,7 @@
 package kr.go.seoul.democracy.discuss.model.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.annotation.Resource;
 
@@ -9,7 +10,6 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import kr.go.seoul.democracy.discuss.model.vo.Discuss;
-import kr.go.seoul.democracy.discuss.model.vo.DiscussComment;
 import kr.go.seoul.democracy.discuss.model.vo.DiscussFile;
 
 @Repository
@@ -31,14 +31,14 @@ public class DiscussDAO {
       return sqlSession.selectOne("discuss.onePost",discussNo);
    }
 
-   public ArrayList<DiscussComment> proComment(int discussNo,int pageSize,int currentCommentPage) {
+   public ArrayList<HashMap<String,Object>> proComment(int discussNo,int pageSize,int currentCommentPage) {
       RowBounds rb=new RowBounds((currentCommentPage-1)*pageSize,pageSize);
-      return new ArrayList<DiscussComment>(sqlSession.selectList("discuss.proCommentList",discussNo,rb));
+      return new ArrayList<HashMap<String,Object>>(sqlSession.selectList("discuss.proCommentList",discussNo,rb));
    }
 
-   public ArrayList<DiscussComment> conComment(int discussNo,int pageSize,int currentCommentPage) {
+   public ArrayList<HashMap<String,Object>> conComment(int discussNo,int pageSize,int currentCommentPage) {
       RowBounds rb=new RowBounds((currentCommentPage-1)*pageSize,pageSize);
-      return new ArrayList<DiscussComment>(sqlSession.selectList("discuss.conCommentList",discussNo,rb));
+      return new ArrayList<HashMap<String,Object>>(sqlSession.selectList("discuss.conCommentList",discussNo,rb));
    }
 
    public int commentTotalCount(int discussNo) {
