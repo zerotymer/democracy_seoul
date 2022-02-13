9b869a39d5f5be7b08c4162bb4246f6c42fd18f6
kimjeao <kimjeao@gmail.com>
2022-02-13 23:49:02 +0900

    HELP

--- src/main/java/kr/go/seoul/democracy/suggest/controller/SugController.java
+++ src/main/java/kr/go/seoul/democracy/suggest/controller/SugController.java
@@ -1,23 +1,38 @@
 package kr.go.seoul.democracy.suggest.controller;
 
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
+import kr.go.seoul.democracy.common.model.vo.Member;
 import kr.go.seoul.democracy.suggest.service.SugService;
+import kr.go.seoul.democracy.suggest.vo.Sug;
 
 
 @Controller
 public class SugController {
-    /// FIELDs
-    private SugService sugService;
 
-    /// CONSTRUCTORs
-    @Autowired
-    public SugController(@Qualifier("sugServiceImpl") SugService sugService) {
-        this.sugService = sugService;
-    }
+	//log
+	private static final Logger log = LoggerFactory.getLogger(SugController.class);
+	
+	@Autowired
+	@Qualifier("sugServiceImpl")
+	private SugService sService;
+	
+
+   
 	
 	/*
 	 * /// METHODs
@@ -27,9 +42,15 @@ public class SugController {
 	 * System.out.println(sugService.selectSysdate());
 	 * System.out.println("SugController - sug() end"); return "index"; }
 	 */
+	
+	
+	
+	
+	
     //글의 리스트를 보는 게시판
     @RequestMapping("/suggest/suggest.do")
     public String sug() {
+    	System.out.println("/////////////////////////////////");
         return "suggest/sugBoard";
     }
     //글을 작성하는곳
@@ -47,6 +68,54 @@ public class SugController {
     public String sug3() {
         return "suggest/sugTest";
     }
+    
+    
+    
+    
+    
+    
+    
+  //List Call
+  	@RequestMapping(value="/suggest/suggest.do", method = RequestMethod.GET)
+  	public ModelAndView sugBoard(ModelAndView mav,
+  			@RequestParam(required=false, defaultValue="1")int curPage) throws Exception{
+  			
+  		int recordCountPage = 9;
+  		List<Sug> list = sService.selectAllList(curPage,recordCountPage);
+  		mav.setViewName("suggest/sugBoard");
+  		mav.addObject("list",list);
+  		return mav;
+  	}
+    
+    
+    
+ // Board Call , reply 
+ 	@RequestMapping(value="/suggest/sugView.do", method = RequestMethod.GET)
+ 	public ModelAndView sugview(		
+ 		@RequestParam(value="suggestNo",defaultValue="1")int suggestNo,
+ 		HttpSession session) throws Exception {
+ 	
+ 		
+ //BoardNum Call
+ 		
+ 		Sug sug = sService.sugView(suggestNo);
+ 			
+ 		ModelAndView mav = new ModelAndView();
+ 		mav.setViewName("suggest/sugView");
+ 		mav.addObject("sug", sService.sugView(suggestNo));
+
+ 		return mav; 
+ 	}
+ 	
+ 	@RequestMapping(value="/suggest/suggestview.do", method=RequestMethod.POST)
+	public String comment(@RequestParam int suggestNo,@RequestParam String userId,Sug sugComment, @SessionAttribute("user") Member member) throws Exception{ 
+ 		sService.comWrite(sugComment);
+		return "redirect:/suggest/sugView?userId="+member.getUserId();
+	}
+	
+	
+    
+    
     
 	/*
 	 * @RequestMapping("/member.do") public String member() { Member m = new
